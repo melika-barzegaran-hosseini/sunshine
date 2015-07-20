a3eeb44490f15fadc903db7ec5ef4ac0d15a049a
Melika Barzegaran Hosseini <melika.barzegaran.hosseini@gmail.com>
2015-07-20 23:06:37 +0430

    Add toast on item click

--- app/src/main/java/app/com/example/android/sunshine/ForecastFragment.java
+++ app/src/main/java/app/com/example/android/sunshine/ForecastFragment.java
@@ -11,8 +11,10 @@ import android.view.MenuInflater;
 import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
+import android.widget.Toast;
 
 import org.json.JSONArray;
 import org.json.JSONException;
@@ -53,8 +55,18 @@ public class ForecastFragment extends Fragment
         View rootView = inflater.inflate(R.layout.fragment_main, container, false);
 
         //gets a reference to the ListView, and attach the Adapter to it.
-        ListView listView = (ListView) rootView.findViewById(R.id.listview_forecast);
+        final ListView listView = (ListView) rootView.findViewById(R.id.listview_forecast);
         listView.setAdapter(this.forecastAdapter);
+
+        listView.setOnItemClickListener(new AdapterView.OnItemClickListener()
+        {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id)
+            {
+                String message = forecastAdapter.getItem(position);
+                Toast.makeText(getActivity(), message, Toast.LENGTH_SHORT).show();
+            }
+        });
 
         return rootView;
     }
