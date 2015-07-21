b7f5ed4c33f8510012c01352e255a0b6dce9a7a9
Melika Barzegaran Hosseini <melika.barzegaran.hosseini@gmail.com>
2015-07-21 20:32:13 +0430

    Add ShareActionProvider to detail fragment menu

--- app/src/main/java/app/com/example/android/sunshine/DetailActivityFragment.java
+++ app/src/main/java/app/com/example/android/sunshine/DetailActivityFragment.java
@@ -3,14 +3,26 @@ package app.com.example.android.sunshine;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.support.v4.view.MenuItemCompat;
+import android.support.v7.widget.ShareActionProvider;
+import android.util.Log;
 import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.MenuInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.TextView;
 
 public class DetailActivityFragment extends Fragment
 {
-    public DetailActivityFragment(){}
+    private static final String LOG_TAG = DetailActivityFragment.class.getSimpleName();
+    private static final String FORECAST_SHARE_HASHTAG = " #SunshineApp";
+    private String message;
+
+    public DetailActivityFragment()
+    {
+        this.setHasOptionsMenu(true);
+    }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
@@ -21,11 +33,39 @@ public class DetailActivityFragment extends Fragment
         Intent intent = getActivity().getIntent();
         if(intent != null && intent.hasExtra(Intent.EXTRA_TEXT))
         {
-            String message = intent.getStringExtra(Intent.EXTRA_TEXT);
+            this.message = intent.getStringExtra(Intent.EXTRA_TEXT);
             TextView textView = (TextView) rootView.findViewById(R.id.detail_text);
-            textView.setText(message);
+            textView.setText(this.message);
         }
 
         return rootView;
+    }
+
+    @Override
+    public void onCreateOptionsMenu(Menu menu, MenuInflater inflater)
+    {
+        inflater.inflate(R.menu.menu_detail, menu);
+
+        ShareActionProvider shareActionProvider = (ShareActionProvider) MenuItemCompat
+                .getActionProvider(menu.findItem(R.id.action_share));
+
+        if (shareActionProvider != null)
+        {
+            shareActionProvider.setShareIntent(createShareIntent());
+        }
+        else
+        {
+            Log.d(LOG_TAG, "share action provider is null.");
+        }
+    }
+
+    private Intent createShareIntent()
+    {
+        Intent shareIntent = new Intent()
+                .setAction(Intent.ACTION_SEND)
+                .setFlags(Intent.FLAG_ACTIVITY_CLEAR_WHEN_TASK_RESET)
+                .setType("text/plain")
+                .putExtra(Intent.EXTRA_TEXT, message + FORECAST_SHARE_HASHTAG);
+        return shareIntent;
     }
 }
