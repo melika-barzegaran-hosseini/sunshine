1081979a794f96005581278318614a38f3496908
Melika Barzegaran Hosseini <melika.barzegaran.hosseini@gmail.com>
2015-07-21 19:45:00 +0430

    Add map intent from new menu item

--- app/src/main/java/app/com/example/android/sunshine/MainActivity.java
+++ app/src/main/java/app/com/example/android/sunshine/MainActivity.java
@@ -1,24 +1,29 @@
 package app.com.example.android.sunshine;
 
 import android.content.Intent;
-import android.support.v7.app.ActionBarActivity;
+import android.net.Uri;
 import android.os.Bundle;
+import android.preference.PreferenceManager;
+import android.support.v7.app.ActionBarActivity;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 
-
-public class MainActivity extends ActionBarActivity {
+public class MainActivity extends ActionBarActivity
+{
+    private final String LOG_TAG = MainActivity.class.getSimpleName();
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(Bundle savedInstanceState)
+    {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
 
 
     @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
+    public boolean onCreateOptionsMenu(Menu menu)
+    {
         getMenuInflater().inflate(R.menu.menu, menu);
         return true;
     }
@@ -33,8 +38,35 @@ public class MainActivity extends ActionBarActivity {
             case R.id.action_settings:
                 startActivity(new Intent(this, SettingsActivity.class));
                 return true;
+
+            case R.id.action_map_location:
+                openPreferredLocationInMap();
+                return true;
         }
 
         return super.onOptionsItemSelected(item);
+    }
+
+    private void openPreferredLocationInMap()
+    {
+        String currentLocation = PreferenceManager
+                .getDefaultSharedPreferences(this)
+                .getString(
+                        getString(R.string.pref_location_key),
+                        getString(R.string.pref_location_default));
+
+        Uri geoLocation = Uri.parse("geo:0,0?").buildUpon()
+                .appendQueryParameter("q", currentLocation)
+                .build();
+
+        Intent intent = new Intent(Intent.ACTION_VIEW).setData(geoLocation);
+        if (intent.resolveActivity(getPackageManager()) != null)
+        {
+            startActivity(intent);
+        }
+        else
+        {
+            Log.d(LOG_TAG, "Couldn't call " + currentLocation + ", no receiving apps is installed!");
+        }
     }
 }
