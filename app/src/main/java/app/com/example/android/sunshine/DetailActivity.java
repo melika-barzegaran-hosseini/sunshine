de002f3806dec2fb3fa2eda885e9739c3aba4666
Melika Barzegaran Hosseini <melika.barzegaran.hosseini@gmail.com>
2015-07-21 15:24:46 +0430

    Launch the settings activity when the settings menu item is selected

--- app/src/main/java/app/com/example/android/sunshine/DetailActivity.java
+++ app/src/main/java/app/com/example/android/sunshine/DetailActivity.java
@@ -1,5 +1,6 @@
 package app.com.example.android.sunshine;
 
+import android.content.Intent;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.view.Menu;
@@ -23,15 +24,15 @@ public class DetailActivity extends ActionBarActivity {
     }
 
     @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
+    public boolean onOptionsItemSelected(MenuItem item)
+    {
         int id = item.getItemId();
 
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
+        switch(id)
+        {
+            case R.id.action_settings:
+                startActivity(new Intent(this, SettingsActivity.class));
+                return true;
         }
 
         return super.onOptionsItemSelected(item);
