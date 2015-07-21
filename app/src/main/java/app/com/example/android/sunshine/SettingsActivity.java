acfec63610d1d19f15005a13c4046d5c1ee04ffc
Melika Barzegaran Hosseini <melika.barzegaran.hosseini@gmail.com>
2015-07-21 14:30:04 +0430

    Add new SettingsActivity

---
+++ app/src/main/java/app/com/example/android/sunshine/SettingsActivity.java
@@ -0,0 +1,56 @@
+package app.com.example.android.sunshine;
+
+import android.os.Bundle;
+import android.preference.ListPreference;
+import android.preference.Preference;
+import android.preference.PreferenceActivity;
+import android.preference.PreferenceManager;
+
+
+public class SettingsActivity extends PreferenceActivity implements Preference.OnPreferenceChangeListener
+{
+    @Override
+    protected void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+
+        // Add 'general' preferences, defined in the XML file
+        // TODO: Add preferences from XML file
+
+        // For all preferences, attach an OnPreferenceChangeListener so the UI summary can be
+        // updated when the preference changes.
+        // TODO: Add preference
+    }
+
+    private void bindPreferenceSummaryToValue(Preference preference)
+    {
+        preference.setOnPreferenceChangeListener(this);
+
+        this.onPreferenceChange(preference, PreferenceManager
+                .getDefaultSharedPreferences
+                        (preference.getContext()).getString(preference.getKey(), ""));
+    }
+
+    @Override
+    public boolean onPreferenceChange(Preference preference, Object value)
+    {
+        String stringValue = value.toString();
+
+        if(preference instanceof ListPreference)
+        {
+            ListPreference listPreference = (ListPreference) preference;
+            int preferenceIndex = listPreference.findIndexOfValue(stringValue);
+
+            if (preferenceIndex >= 0)
+            {
+                preference.setSummary(listPreference.getEntries()[preferenceIndex]);
+            }
+        }
+        else
+        {
+            preference.setSummary(stringValue);
+        }
+
+        return true;
+    }
+}
