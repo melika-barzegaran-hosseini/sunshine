2b531cc12c85bd160c5caa3ff000e3e4cac1eab4
Melika Barzegaran Hosseini <melika.barzegaran.hosseini@gmail.com>
2015-07-28 10:08:03 +0430

    Register the content provider

--- app/src/androidTest/java/app/com/example/android/sunshine/data/TestProvider.java
+++ app/src/androidTest/java/app/com/example/android/sunshine/data/TestProvider.java
@@ -1,21 +1,9 @@
-/*
- * Copyright (C) 2014 The Android Open Source Project
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *      http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
 package app.com.example.android.sunshine.data;
 
+import android.content.ComponentName;
 import android.content.ContentValues;
+import android.content.pm.PackageManager;
+import android.content.pm.ProviderInfo;
 import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
 import android.test.AndroidTestCase;
@@ -23,15 +11,8 @@ import android.test.AndroidTestCase;
 import app.com.example.android.sunshine.data.WeatherContract.LocationEntry;
 import app.com.example.android.sunshine.data.WeatherContract.WeatherEntry;
 
-/*
-    Note: This is not a complete set of tests of the Sunshine ContentProvider, but it does test
-    that at least the basic functionality has been implemented correctly.
-
-    Students: Uncomment the tests in this class as you implement the functionality in your
-    ContentProvider to make sure that you've implemented things reasonably correctly.
- */
-public class TestProvider extends AndroidTestCase {
-
+public class TestProvider extends AndroidTestCase
+{
     public static final String LOG_TAG = TestProvider.class.getSimpleName();
 
     /*
@@ -106,32 +87,28 @@ public class TestProvider extends AndroidTestCase {
         deleteAllRecords();
     }
 
-    /*
-        This test checks to make sure that the content provider is registered correctly.
-        Students: Uncomment this test to make sure you've correctly registered the WeatherProvider.
-     */
-//    public void testProviderRegistry() {
-//        PackageManager pm = mContext.getPackageManager();
-//
-//        // We define the component name based on the package name from the context and the
-//        // WeatherProvider class.
-//        ComponentName componentName = new ComponentName(mContext.getPackageName(),
-//                WeatherProvider.class.getName());
-//        try {
-//            // Fetch the provider info using the component name from the PackageManager
-//            // This throws an exception if the provider isn't registered.
-//            ProviderInfo providerInfo = pm.getProviderInfo(componentName, 0);
-//
-//            // Make sure that the registered authority matches the authority from the Contract.
-//            assertEquals("Error: WeatherProvider registered with authority: " + providerInfo.authority +
-//                    " instead of authority: " + WeatherContract.CONTENT_AUTHORITY,
-//                    providerInfo.authority, WeatherContract.CONTENT_AUTHORITY);
-//        } catch (PackageManager.NameNotFoundException e) {
-//            // I guess the provider isn't registered correctly.
-//            assertTrue("Error: WeatherProvider not registered at " + mContext.getPackageName(),
-//                    false);
-//        }
-//    }
+    public void testProviderRegistry()
+    {
+        PackageManager packageManager = this.getContext().getPackageManager();
+
+        // We define the component name based on the package name from the context and the
+        // WeatherProvider class.
+        ComponentName componentName = new ComponentName(this.getContext().getPackageName(),
+                WeatherProvider.class.getName());
+        try
+        {
+            ProviderInfo providerInfo = packageManager.getProviderInfo(componentName, 0);
+            assertEquals("ERROR: WeatherProvider registered with authority: " +
+                            providerInfo.authority + " instead of authority: " +
+                            WeatherContract.CONTENT_AUTHORITY, providerInfo.authority,
+                    WeatherContract.CONTENT_AUTHORITY);
+        }
+        catch (PackageManager.NameNotFoundException e)
+        {
+            assertTrue("ERROR: WeatherProvider is not registered at " + this.getContext()
+                            .getPackageName(), false);
+        }
+    }
 
     /*
             This test doesn't touch the database.  It verifies that the ContentProvider returns
