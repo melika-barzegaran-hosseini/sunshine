ba0e3b37fc5d26bfdbea1a6928894e72a6a946c0
Melika Barzegaran Hosseini <melika.barzegaran.hosseini@gmail.com>
2015-08-19 16:15:34 +0430

    Add RetentionMode, IntDef and interface notations

--- app/src/main/java/app/com/example/android/sunshine/sync/SunshineSyncAdapter.java
+++ app/src/main/java/app/com/example/android/sunshine/sync/SunshineSyncAdapter.java
@@ -21,6 +21,7 @@ import android.net.Uri;
 import android.os.Build;
 import android.os.Bundle;
 import android.preference.PreferenceManager;
+import android.support.annotation.IntDef;
 import android.support.v4.app.NotificationCompat;
 import android.support.v4.app.TaskStackBuilder;
 import android.util.Log;
@@ -33,6 +34,8 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
 import java.net.HttpURLConnection;
 import java.net.URL;
 import java.util.Calendar;
@@ -46,6 +49,14 @@ import app.com.example.android.sunshine.data.WeatherContract;
 public class SunshineSyncAdapter extends AbstractThreadedSyncAdapter
 {
     public final String LOG_TAG = SunshineSyncAdapter.class.getSimpleName();
+
+    @Retention(RetentionPolicy.SOURCE)
+    @IntDef({STATUS_OK, STATUS_SERVER_DOWN, STATUS_SERVER_INVALID, STATUS_UNKNOWN})
+    public @interface Status{}
+    public static final int STATUS_OK = 0;
+    public static final int STATUS_SERVER_DOWN = 1;
+    public static final int STATUS_SERVER_INVALID = 2;
+    public static final int STATUS_UNKNOWN = 4;
 
     private static final String[] NOTIFY_WEATHER_PROJECTION = new String[]{
             WeatherContract.WeatherEntry.COLUMN_WEATHER_ID,
